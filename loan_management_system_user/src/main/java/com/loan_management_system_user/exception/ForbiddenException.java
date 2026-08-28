fe3c49f25adf5aefb019d64e4821f71ba0c7005d
Sujitclear <adsulsujit2006@gmail.com>
2026-08-28 18:47:45 +0530

    Add customeexception and Global Exception handling

---
+++ loan_management_system_user/src/main/java/com/loan_management_system_user/exception/ForbiddenException.java
@@ -0,0 +1,10 @@
+package com.loan_management_system_user.exception;
+
+public class ForbiddenException extends RuntimeException{
+	public ForbiddenException(String message){
+		super(message);
+	}
+
+
+}
+
