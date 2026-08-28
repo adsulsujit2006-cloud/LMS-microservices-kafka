2d56eecc9623454aa9b24a2a065e58187c61b86b
Sujitclear <adsulsujit2006@gmail.com>
2026-08-28 17:57:55 +0530

    Complete modal class and requried enums

---
+++ src/main/java/com/loan_management_system_user/enums/ModuleType.java
@@ -0,0 +1,25 @@
+package com.loan_management_system_user.enums;
+
+public enum ModuleType {
+	 USER,
+
+	    BRANCH,
+
+	    ROLE,
+
+	    PERMISSION,
+
+	    LOAN_TYPE,
+
+	    LOAN_APPLICATION,
+
+	    LOAN,
+
+	    EMI,
+
+	    PAYMENT,
+
+	    REPORT;
+
+
+}
---
+++ src/main/java/com/loan_management_system_user/enums/RoleType.java
@@ -0,0 +1,21 @@
+package com.loan_management_system_user.enums;
+
+
+public enum RoleType {
+	SUPER_ADMIN,
+
+    ADMIN,
+
+    BRANCH_MANAGER,
+
+    REPRESENTATIVE_EXECUTIVE,
+    OPERATIONS_MANAGER,
+    CREDIT_MANAGER,
+
+    CUSTOMER,
+
+    AUDITOR,
+
+    COLLECTION_AGENT
+
+}
